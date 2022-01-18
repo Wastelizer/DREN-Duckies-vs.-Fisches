fefdcdfbd910654531ed1d36f1cee0cd15327ae0
Thomas Brandl <thomas.brandl@st.oth-regensburg.de>
2022-01-18 17:08:59 +0100

    Dataimport: Add script for data import and visualisation
    import_csv.py:
    - read the historical sales data from the CSV-File and visualice it in a plot
    - export the plot as a pdf-File
    
    Repro_Paper:
    - inlude the grafic from the dateimport-script into the Latex-Paper

--- source/import_csv.py
+++ source/import_csv.py
@@ -1,13 +1,45 @@
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
-filename = 'historical_sales_data.csv'
+filename = '../data/historical_sales_data.csv'
+time = []
+fish = []
+ducks = []
+total = []
+month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-with open(filename) as csv_file:
-    reader = csv.reader(csv_file, delimiter=';')
-    header_row = next(reader)
+def dataImport():
+    with open(filename) as csv_file:
+        reader = csv.reader(csv_file, delimiter=';')
+        header_row = next(reader)
+        i = 0
+        for row in reader:
+            if ((i%4) == 0):
+                time.append(month[i] + row[1][2:4])
+            else :
+                time.append('');
+            i = ((i+1) % 12)
+            fish.append(int(row[2]))
+            ducks.append(int(row[3]))
+            total.append(int(row[4]))
+        
     
-for index, column_header in enumerate(header_row):
-    print(index, column_header)
+def dataVisualisation():
+    x = np.arange(0, len(fish), 1)
+    plt.plot(x, fish, color = 'b', label = 'Fishes')
+    plt.plot(x, ducks, color = 'g', label = 'Ducks')
+    plt.plot(x, total, color = 'r', label = 'Total')
+    plt.xticks(x, time, rotation = 45)
+    plt.title('Fig 3: Historical data of sales')
+    plt.ylabel('number of sales')
+    #plt.xlabel('time')
+    plt.subplots_adjust(bottom=0.2)
+    plt.margins(x=0)
+    plt.grid()
+    plt.legend()
+    plt.savefig('../Paper/fig_historical_sales.pdf', format='pdf')
+    #plt.show()
     
-
+dataImport()
+dataVisualisation()
