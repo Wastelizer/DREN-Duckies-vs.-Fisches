5f19a6d73cb59458cd705ce4895318939a244684
Wastelizer <sebastian.sippl@gmx.de>
2022-02-01 17:18:55 +0100

    Data visualization: generating plots for feasible regaion Paper: add the referenze of the book and plot feasible region

--- source/import_csv.py
+++ source/import_csv.py
@@ -25,21 +25,52 @@ def dataImport():
             total.append(int(row[4]))
         
     
-def dataVisualisation():
-    x = np.arange(0, len(fish), 1)
-    plt.plot(x, fish, color = 'b', label = 'Fishes')
-    plt.plot(x, ducks, color = 'g', label = 'Ducks')
-    plt.plot(x, total, color = 'r', label = 'Total')
-    plt.xticks(x, time, rotation = 45)
-    plt.title('Fig 3: Historical data of sales')
-    plt.ylabel('number of sales')
-    #plt.xlabel('time')
-    plt.subplots_adjust(bottom=0.2)
-    plt.margins(x=0)
-    plt.grid()
-    plt.legend()
-    plt.savefig('../paper/fig_historical_sales.pdf', format='pdf')
-    #plt.show()
+def plot_historic_sales():
+	plt.figure(1)
+	x = np.arange(0, len(fish), 1)
+	plt.plot(x, fish, color = 'b', label = 'Fishes')
+	plt.plot(x, ducks, color = 'g', label = 'Ducks')
+	plt.plot(x, total, color = 'r', label = 'Total')
+	plt.xticks(x, time, rotation = 45)
+	#plt.title('Fig 3: Historical data of sales')
+	plt.ylabel('number of sales')
+	#plt.xlabel('time')
+	plt.subplots_adjust(bottom=0.2)
+	plt.margins(x=0)
+	plt.grid()
+	plt.legend()
+	plt.savefig('../paper/fig_historical_sales.pdf', format='pdf')
+	#plt.show()
+    
+#plot the feasible region with the variables from the book
+def plot_feasible_region():
+
+	
+	plt.figure(2)
+	#plot time constraint
+	plt.axhline(y=400, linestyle='--', color = 'b', label='time ducks')
+	plt.axvline(x=300, linestyle='--', color = 'r', label='time fishes')
+	plt.ylim(0, 500)
+	plt.xlim(0, 500)
+	
+	#plot pallets constraint
+	x_values = [400, 0]
+	y_values = [0, 500]
+	plt.plot(x_values, y_values, linestyle='--', color = 'g', label='rubber supply')
+	
+	#fill green color in the feasible region
+	x = np.arange(0,300,1) 
+	m = -500/400
+	y1 = m*x + 500
+	y = np.minimum(y1, 400)
+	plt.fill_between(x, y, color='lime')
+	
+	plt.ylabel('ducks')
+	plt.xlabel('fishes')
+	plt.scatter(80, 400, marker='o', color='k', label='product mix')
+	plt.legend(loc=5)
+	plt.savefig('../paper/feasible_region.pdf', format='pdf')
     
 dataImport()
-dataVisualisation()
+plot_historic_sales()
+plot_feasible_region()
--- source/solver.py
+++ source/solver.py
@@ -1,5 +1,7 @@
-
 import argparse as arg
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 #define commandline arguments to parse constraints to the solver
 cli = arg.ArgumentParser(description='Add the Constraints for the Solver')
@@ -7,7 +9,7 @@ cli.add_argument('--profs','-p', type=int, nargs=2, default=[5,4], help='profit 
 cli.add_argument('--times','-t', type=int, nargs=2, default=[400,300],help='available production time for duckies (D), fishes (F) (default: %(default)s)',  metavar=('D', 'F'))
 cli.add_argument('--palls','-e', type=int, nargs=2, default=[100,125], help='required pallets per duck (D), fish (F) (default: %(default)s)', metavar=('D', 'F'))
 cli.add_argument('--rubbs','-r', type=int, nargs=1, default=50000, help='total supplied rubber pallets (N) (default:%(default)s)', metavar=('N'))
-cli.add_argument('--assum','-a', type=int, nargs=2, default=[400,300], help='assumption for future sales duckies (D), fishes(F) (default: %(default)s)', metavar=('D','F'))
+cli.add_argument('--assum','-a', type=int, nargs=2, help='assumption for future sales duckies (D), fishes(F) (default: %(default)s)', metavar=('D','F'))
 
 args=cli.parse_args()
 
@@ -26,14 +28,45 @@ pallets_per_duck = args.palls[0] #default = 100
 pallets_per_fish = args.palls[1] #default = 125
 
 #constraint assumtion for future sales
-duck_sales = args.assum[0] #default = 400
-fish_sales = args.assum[1] #default = 300
+duck_sales = time_duck
+fish_sales = time_fish
+if(args.assum):
+	duck_sales = args.assum[0] 
+	fish_sales = args.assum[1] 
 
 
 
 #assign the min of time and sales constraint to the max count of duckies and fishes
 max_duck = min(time_duck, duck_sales)
 max_fish = min(time_fish, fish_sales)
+
+
+#plot the feasible region
+def plot_feasible_region():
+	
+	#plot time constraint
+	plt.axhline(y=time_duck, linestyle='--', color = 'b', label='time ducks')
+	plt.axvline(x=time_fish, linestyle='--', color = 'r', label='time fishes')
+	plt.ylim(0, time_duck+100)
+	plt.xlim(0, time_fish+100)
+	
+	#plot pallets constraint
+	x_values = [total_pallets/pallets_per_fish, 0]
+	y_values = [0, total_pallets/pallets_per_duck]
+	plt.plot(x_values, y_values, linestyle='--', color = 'g', label='rubber supply')
+	
+	#fill green color in the feasible region
+	x_range = np.arange(0,time_fish,1) #from 0 to max_fish
+	
+	#points on the pallets constraint line
+	m = (0-total_pallets/pallets_per_duck)/(total_pallets/pallets_per_fish-0)
+	y1 = m*x_range + total_pallets/pallets_per_duck
+	y = np.minimum(y1, time_duck)
+	plt.fill_between(x_range, y, color='lime')
+	
+	plt.ylabel('duckies')
+	plt.xlabel('fishes')
+	#plt.title('feasible region')
 
 
 #the solver to calculate the optimal produkt mix
@@ -53,6 +86,14 @@ def solver():
 					product_mix = [current_profit, count_duck, count_fish]
 	
 	return product_mix
+	
+plot_feasible_region()
+
 print('optimal product mix for the given constraints:')
 print('[total_profit, duckies, fishes]')
-print(solver())
+p_mix = solver()
+print(p_mix)
+
+plt.scatter(p_mix[2], p_mix[1], marker='o', color='k', label='product mix')
+plt.legend()
+plt.savefig('../paper/feasible_region.pdf', format='pdf')
