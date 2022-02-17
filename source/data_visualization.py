e9d5784e75b9e6d3db3fdacc60d32a12217b2f9a
Thomas Brandl <thomas.brandl@st.oth-regensburg.de>
2022-02-17 17:31:43 +0100

    data_visualization: Add commends

--- source/data_visualization.py
+++ source/data_visualization.py
@@ -7,45 +7,53 @@ from pathlib import Path
 path = Path(__file__).absolute()
 repro_dir = str(path.parent.parent)
 
+#arrays for ploting the historical data
 filename = repro_dir + '/data/historical_sales_data.csv'
 time = []
 fish = []
 ducks = []
 total = []
 month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+x_lable_offset = 4 # not every month needs a lable. it is enogth to print every 4th lable
 
+#read the csv-file at 'filename' and load the date in separatly lists
 def dataImport():
     with open(filename) as csv_file:
         reader = csv.reader(csv_file, delimiter=';')
         header_row = next(reader)
         i = 0
-        for row in reader:
-            if ((i%4) == 0):
-                time.append(month[i] + row[1][2:4])
+        for row in reader: # run through the rows of the dataset
+            if ((i%x_lable_offset) == 0):
+                time.append(month[i] + row[1][2:4]) # generate a list with month and year of the data for the x-lable 
             else :
                 time.append('');
             i = ((i+1) % 12)
+            
+            #load the sales date in the lists
             fish.append(int(row[2]))
             ducks.append(int(row[3]))
             total.append(int(row[4]))
         
     
 def plot_historic_sales():
-	plt.figure(1)
+	plt.figure(1) # create a new figure
 	x = np.arange(0, len(fish), 1)
+    # plot datesets
 	plt.plot(x, fish, color = 'b', label = 'Fishes')
 	plt.plot(x, ducks, color = 'g', label = 'Ducks')
 	plt.plot(x, total, color = 'r', label = 'Total')
-	plt.xticks(x, time, rotation = 45)
-	#plt.title('Fig 3: Historical data of sales')
+	# plot x- and y-lable
+	plt.xticks(x, time)
 	plt.ylabel('number of sales')
-	#plt.xlabel('time')
+	# align the plot
 	plt.subplots_adjust(bottom=0.2)
 	plt.margins(x=0)
+    # plot a grid in the figure
 	plt.grid()
+    # plot the legend 
 	plt.legend()
+    #safing the plot as pdf-file
 	plt.savefig(repro_dir + '/paper/fig_historical_sales.pdf', format='pdf')
-	#plt.show()
     
 #plot the feasible region with the variables from the book
 def plot_feasible_region():
